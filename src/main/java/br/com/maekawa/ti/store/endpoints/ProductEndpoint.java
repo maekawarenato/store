d681fe60c7a78ca46918c08a3d02d779cf739ff2
Renato Maekawa <cin_rmaekawa@localhost.localdomain>
2018-03-22 19:29:40 -0300

    Edited Return

--- src/main/java/br/com/maekawa/ti/store/endpoints/ProductEndpoint.java
+++ src/main/java/br/com/maekawa/ti/store/endpoints/ProductEndpoint.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,7 +18,7 @@ public class ProductEndpoint {
     private ProductRepository productRepository;
 
     @ReadOperation
-    public List<?> findAllProducts(ServerHttpRequest request){
+    public ResponseEntity<?> findAllProducts(ServerHttpRequest request){
         return Optional.of(productRepository.findAll())
                 .map(new ResourceBuilder<Product>(request)::build)
                 .map(ResponseEntity.ok()::body)
